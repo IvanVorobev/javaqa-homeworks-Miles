f4883c7c6c2f6560e0ce84bc6382e1cc6df7e475
IvanVorobev <vivancho@list.ru>
2020-05-05 22:01:19 +0300

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 4356;
+        int bonusmilevalue = 20;
+        int bonus = ticketprice / bonusmilevalue;
+        System.out.println(bonus);
+
+    }
+}
